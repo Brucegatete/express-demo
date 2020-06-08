bf3aff883918e3a261e90391273665c1a65ebd7e
Bruce Gatete <gateteb3@gmail.com>
2020-06-08 11:07:13 -0400

    created an express module

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end(' Will send all the dishes to you');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('will send dish: ' + req.body.name + ' with detail ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('deleting all the dishes');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end(' Will send the details of: ' + req.params.dishId + 'to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported for dish : ' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish ' + req.params.dishId + '\n');
+    res.end('Will update dish ' + req.body.name + ' with details ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Serve running at http://${hostname}:${port}/`);
+});
